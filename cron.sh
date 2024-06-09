37dd077c3b9a97fb9e5327be50f1c0adf2ab128d
LeonEstrak <aniket2583@gmail.com>
2024-06-09 20:40:01 +0530

    Changes as of 2024-06-09 20:40:01

--- cron.sh
+++ cron.sh
@@ -3,3 +3,6 @@ directoryPath="/home/aniket/ludusavi-backup"
 ludusavi backup --no-cloud-sync --force --path "$directoryPath"
 
 $directoryPath/backup.sh "$directoryPath"
+
+echo "Script completed at: $(date)"
+echo "----------------------------"
