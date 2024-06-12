e81f1f0ef9b9381862ebbbe46672829de82d9c7c
LeonEstrak <aniket2583@gmail.com>
2024-06-13 00:05:02 +0530

    Changes as of 2024-06-13 00:05:02

--- cron.sh
+++ cron.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -x
 
+echo "\n----------------------------"
+echo "Script started at: $(date)\n"
+
 directoryPath="/home/aniket/ludusavi-backup"
 
 /home/aniket/.cargo/bin/ludusavi backup --no-cloud-sync --force --path "$directoryPath" && \
 bash "$directoryPath"/backup.sh "$directoryPath"
 
-echo "Script completed at: $(date)"
-echo "----------------------------"
+echo "\nScript completed at: $(date)"
+echo "----------------------------\n"
